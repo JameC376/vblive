4ff688c0dc3461e92efa35d686de3c9cee499ede
JameC376 <sarawoot1511@gmail.com>
2026-04-30 15:13:49 +0700

    Update generate_m3u.py

--- generate_m3u.py
+++ generate_m3u.py
@@ -8,20 +8,20 @@ data = requests.get(API).json()
 
 playlist = "#EXTM3U\n"
 
-for item in data["playlist"]:
+items = data["playlist"]["items"]
+
+for item in items:
 
     video_id = item["mediaid"]
     title = item["title"]
 
-    image = ""
-    if "image" in item:
-        image = item["image"]
+    image = item["image"]
 
     stream = BASE_STREAM.format(id=video_id)
 
     playlist += f'#EXTINF:-1 tvg-logo="{image}" group-title="Volleyball",{title}\n'
     playlist += f"{stream}\n"
-
+    
 with open("munin.m3u", "w", encoding="utf-8") as f:
     f.write(playlist)
 
