64ee71b66bb41199d4be3fe99db264558d923008
JameC376 <sarawoot1511@gmail.com>
2026-04-30 15:19:03 +0700

    Update generate_m3u.py

--- generate_m3u.py
+++ generate_m3u.py
@@ -4,24 +4,27 @@ API = "https://tv.volleyballworld.com/api/client-feed?feed-url=https%3A%2F%2Fzap
 
 BASE_STREAM = "https://livecdn.euw1-0005.jwplive.com/live/sites/fM9jRrkn/media/{id}/live.isml/.m3u8"
 
-data = requests.get(API).json()
+r = requests.get(API)
+data = r.json()
 
 playlist = "#EXTM3U\n"
 
-items = data["playlist"]["items"]
+items = data.get("items", [])
 
 for item in items:
 
-    video_id = item["mediaid"]
-    title = item["title"]
+    video_id = item.get("mediaid")
+    title = item.get("title", "Volleyball Live")
 
-    image = item["image"]
+    image = ""
+    if "image" in item:
+        image = item["image"]
 
     stream = BASE_STREAM.format(id=video_id)
 
     playlist += f'#EXTINF:-1 tvg-logo="{image}" group-title="Volleyball",{title}\n'
     playlist += f"{stream}\n"
-    
+
 with open("munin.m3u", "w", encoding="utf-8") as f:
     f.write(playlist)
 
